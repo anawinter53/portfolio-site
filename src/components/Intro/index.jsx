674ccfe94de478ba2ba433fafcd2820a32ee49dd
anawinter53 <anatolia.winter@yahoo.com>
2023-05-30 19:50:02 +0100

    add hero and more design to intro section

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -4,12 +4,27 @@ import './intro.css'
 export default function Intro() {
   return (
     <div className="container" id="intro">
-      {/* <img src='../../../blur-background.webp' className='background-img'></img> */}
-      <div className="col pb-5">
-        <h4 className="fw-light pb-2">Hey!</h4>
-        <h1 className="display-3 pb-2">I'm Anatolia Winter</h1>
-        <h4 className="fw-light pb-2">I'm into Full Stack Development/Testing</h4>
-        <button className="btn btn-dark">About Me</button>
+      <div className='row'>
+        <div className="col col-7 description">
+          <h4 className="fw-light pb-2">Hey!</h4>
+          <h1 className="display-3 pb-2">I'm Anatolia Winter</h1>
+          <h4 className="fw-light pb-2">I'm into Full Stack Development/Testing</h4>
+          <button className="btn btn-dark">About Me</button>
+        </div>
+        <div className='col position-relative intro-images'>
+          <div className='rounded'>
+            <img 
+              src='studying.png'
+              alt='hero image'
+              draggable='false'
+              loading='eager'
+              style={{ maxWidth: "350px"}}
+            />
+          </div>
+          <div className='position-absolute'>
+
+          </div>
+        </div>
       </div>
     </div>
   )
